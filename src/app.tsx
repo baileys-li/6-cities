f8a498c7b9bfdf157c95c5ecaf26c05ecdfa5eb7
Arthur Baileys Li <arthurlitovko@gmail.com>
2023-07-15 14:40:22 +0300

    Use new Router API

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 
 import { AuthorizationStatus, TemporalData } from './constants';
 import { AppRoute } from './constants/routes';
@@ -9,34 +9,38 @@ import { LoginPage } from './pages/login/login-page';
 import { MainPage } from './pages/main/main-page';
 import { OfferPage } from './pages/offer/offer-page';
 
+const authorizationStatus = AuthorizationStatus.Auth;
+
+const router = createBrowserRouter(createRoutesFromElements(
+	<Route>
+		<Route
+			element={<MainPage offersAmount={TemporalData.OfferAmount} />}
+			path={AppRoute.Main}
+		/>
+		<Route
+			element={
+				<PrivateRoute status={authorizationStatus}>
+					<FavoritesPage />
+				</PrivateRoute>
+			}
+			path={AppRoute.Favorites}
+		/>
+		<Route
+			element={
+				<PublicRoute status={authorizationStatus}>
+					<LoginPage />
+				</PublicRoute>
+			}
+			path={AppRoute.Login}
+		/>
+		<Route element={<OfferPage />} path={AppRoute.Offer} />
+		<Route element={<Page404 />} path="*" />
+	</Route>
+));
+
 export function App() {
-	const authorizationStatus = AuthorizationStatus.Auth;
+
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route
-					element={<MainPage offersAmount={TemporalData.OfferAmount} />}
-					path={AppRoute.Main}
-				/>
-				<Route
-					element={
-						<PrivateRoute status={authorizationStatus}>
-							<FavoritesPage />
-						</PrivateRoute>
-					}
-					path={AppRoute.Favorites}
-				/>
-				<Route
-					element={
-						<PublicRoute status={authorizationStatus}>
-							<LoginPage />
-						</PublicRoute>
-					}
-					path={AppRoute.Login}
-				/>
-				<Route element={<OfferPage />} path={AppRoute.Offer} />
-				<Route element={<Page404 />} path="*" />
-			</Routes>
-		</BrowserRouter>
+		<RouterProvider router={router} />
 	);
 }
