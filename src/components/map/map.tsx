3275e64240fe38de41f36cbc24b9ed484bc4f4d7
Arthur Baileys Li <arthurlitovko@gmail.com>
2023-07-22 14:38:33 +0300

    Add simple leaflet example

---
+++ src/components/map/map.tsx
@@ -0,0 +1,50 @@
+import classNames from 'classnames';
+import {Marker, layerGroup} from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import {useEffect, useRef} from 'react';
+
+import type { ServerLocation, ServerOffer } from '../../types/offer';
+
+import useMap from '../../hooks/map';
+import { activeIcon, defaultIcon } from './icons';
+
+interface MapProps {
+  activeId: null | string;
+	className?: string;
+  location: ServerLocation;
+	offers: ServerOffer[];
+}
+
+
+export function Map({activeId, className, location, offers}: MapProps): JSX.Element {
+
+	const mapRef = useRef(null);
+	const map = useMap(mapRef, location);
+
+	useEffect(() => {
+		if (map) {
+			const markerLayer = layerGroup().addTo(map);
+			offers.forEach((offer) => {
+				const marker = new Marker({
+					lat: offer.location.latitude,
+					lng: offer.location.longitude
+				});
+
+				marker
+					.setIcon(
+						activeId === offer.id
+							? activeIcon
+							: defaultIcon
+					)
+					.addTo(markerLayer);
+			});
+
+			return () => {
+				map.removeLayer(markerLayer);
+			};
+		}
+	}, [map, offers, activeId]);
+
+	return <div className={classNames(className, 'map')} ref={mapRef}></div>;
+}
+
