862404b8f84995c40b9a67468bf84c338da0a64d
Arthur Baileys Li <arthurlitovko@gmail.com>
2023-08-22 16:17:10 +0300

    Use custom selector for auth

--- src/hooks/is-authorized.ts
+++ src/hooks/is-authorized.ts
@@ -1,8 +1,13 @@
+import { createSelector } from '@reduxjs/toolkit';
+
+import type { Store } from '../types/store';
+
 import { AuthorizationStatus } from '../constants';
 import { useAppSelector } from './store';
 
-export function useAuth() {
-	const status = useAppSelector((state) => state.user.status);
-	return status === AuthorizationStatus.Auth;
-}
+const selectAuthStatus = (state: Store) => state.user.status;
+const selectIsAuth = createSelector(selectAuthStatus, (status) => status === AuthorizationStatus.Auth);
 
+export function useAuth() {
+	return useAppSelector(selectIsAuth);
+}
