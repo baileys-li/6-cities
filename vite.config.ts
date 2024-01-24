401a56ac1cd255267e0ff8c655c08d7f1b0af3f9
Arthur Baileys Li <arthurlitovko@gmail.com>
2024-01-24 21:13:05 +0200

    Switch postcss ➡️ lightning css

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,21 @@
 /// <reference types='vite/client' />
 
 import react from '@vitejs/plugin-react';
+import browserslist from 'browserslist';
+import { browserslistToTargets } from 'lightningcss';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	css: {
+		lightningcss: {
+			drafts: {
+				customMedia: true,
+			},
+			targets: browserslistToTargets(browserslist('defaults'))
+		},
+		transformer: 'lightningcss',
+	},
 	plugins: [react()],
 	preview: {
 		port: 5173,
